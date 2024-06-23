914b7593be90193f33dcfea6d98b4c092e275e50
SaintHazzard <chamitos1998@hotmail.com>
2024-06-23 11:24:21 -0500

    PlaneModelService.java file has been modified

--- src/main/java/com/vuelosjanbi/planeModel/application/PlaneModelService.java
+++ src/main/java/com/vuelosjanbi/planeModel/application/PlaneModelService.java
@@ -1,5 +1,36 @@
 package com.vuelosjanbi.planeModel.application;
 
+import com.vuelosjanbi.planeModel.application.ports.out.PlaneModelRepositoryPort;
+import com.vuelosjanbi.planeModel.domain.models.PlaneModel;
+
+import java.util.Optional;
+import java.util.List;
+
 public class PlaneModelService {
 
+  private PlaneModelRepositoryPort planeModelRepositoryPort;
+
+  public PlaneModelService(PlaneModelRepositoryPort planeModelRepositoryPort) {
+    this.planeModelRepositoryPort = planeModelRepositoryPort;
+  }
+
+  public PlaneModel createPlaneModel(PlaneModel planeModel) {
+    return planeModelRepositoryPort.save(planeModel);
+  }
+
+  public Optional<PlaneModel> getPlaneModelById(Long id) {
+    return planeModelRepositoryPort.findById(id);
+  }
+
+  public PlaneModel updatePlaneModel(PlaneModel planeModel) {
+    return planeModelRepositoryPort.save(planeModel);
+  }
+
+  public void deletePlaneModel(Long id) {
+    planeModelRepositoryPort.deleteById(id);
+  }
+
+  public List<PlaneModel> getAllPlaneModels() {
+    return planeModelRepositoryPort.findAll();
+  }
 }
