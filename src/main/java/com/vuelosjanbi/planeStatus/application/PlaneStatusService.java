bae38dd9562483fe0ffbb3ebe98d3f1c65759cf1
SaintHazzard <chamitos1998@hotmail.com>
2024-06-23 11:32:50 -0500

    PlaneStatusService.java file has been modified

--- src/main/java/com/vuelosjanbi/planeStatus/application/PlaneStatusService.java
+++ src/main/java/com/vuelosjanbi/planeStatus/application/PlaneStatusService.java
@@ -1,5 +1,36 @@
 package com.vuelosjanbi.planeStatus.application;
 
+import com.vuelosjanbi.planeStatus.application.ports.out.PlaneStatusRepositoryPort;
+import com.vuelosjanbi.planeStatus.domain.models.PlaneStatus;
+import java.util.Optional;
+import java.util.List;
+
 public class PlaneStatusService {
+
+  private PlaneStatusRepositoryPort planeStatusRepositoryPort;
+
+  public PlaneStatusService(PlaneStatusRepositoryPort planeStatusRepositoryPort) {
+    this.planeStatusRepositoryPort = planeStatusRepositoryPort;
+  }
+
+  public PlaneStatus createPlaneStatus(PlaneStatus planeStatus) {
+    return planeStatusRepositoryPort.save(planeStatus);
+  }
+
+  public Optional<PlaneStatus> getPlaneStatusById(Long id) {
+    return planeStatusRepositoryPort.findById(id);
+  }
+
+  public PlaneStatus updatePlaneStatus(PlaneStatus planeStatus) {
+    return planeStatusRepositoryPort.save(planeStatus);
+  }
+
+  public void deletePlaneStatus(Long id) {
+    planeStatusRepositoryPort.deleteById(id);
+  }
+
+  public List<PlaneStatus> getAllPlaneStatus() {
+    return planeStatusRepositoryPort.findAll();
+  }
 
 }
