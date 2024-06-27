df10d34b51a40ddc2a0ee771a26fd900c3a4e408
JpCardenasG <jpcardenasgonzalez.com>
2024-06-27 17:48:01 -0500

    fix: :bug: Added missing annotations
    @traningLeader

--- src/main/java/com/vuelosjanbi/trip/infrastructure/adapters/in/TripConsoleAdapter.java
+++ src/main/java/com/vuelosjanbi/trip/infrastructure/adapters/in/TripConsoleAdapter.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 
 import java.util.Scanner;
 import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 
 import com.vuelosjanbi.airport.application.AirportService;
