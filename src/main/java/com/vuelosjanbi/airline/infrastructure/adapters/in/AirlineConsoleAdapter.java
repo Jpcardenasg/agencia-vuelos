2e8a3a279435a3f9419456258543d42e37180cad
JpCardenasG <jpcardenasgonzalez.com>
2024-07-01 17:10:53 -0500

    refactor: :lipstick: Minor changes
    @traningLeader

--- src/main/java/com/vuelosjanbi/airline/infrastructure/adapters/in/AirlineConsoleAdapter.java
+++ src/main/java/com/vuelosjanbi/airline/infrastructure/adapters/in/AirlineConsoleAdapter.java
@@ -10,99 +10,87 @@ import com.vuelosjanbi.airline.domain.models.Airline;
 @Controller
 public class AirlineConsoleAdapter {
 
-  @Autowired
-  private AirlineService airlineService;
+    @Autowired
+    private AirlineService airlineService;
 
-  public void start() {
-    System.out.println("\n");
-    System.out.println("1. Create Airline");
-    System.out.println("2. Update Airline");
-    System.out.println("3. Delete Airline");
-    System.out.println("4. Get Airline by Id");
-    System.out.println("5. Get Airline by Name");
-    System.out.println("6. Get All Airlines");
-    System.out.println("7. Exit");
+    public void start() {
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-      System.out.println("1. Create Airline");
-      System.out.println("2. Update Airline");
-      System.out.println("3. Delete Airline");
-      System.out.println("4. Get Airline by Id");
-      System.out.println("5. Get Airline by Name");
-      System.out.println("6. Get All Airlines");
-      System.out.println("7. Exit");
+        while (true) {
+            System.out.println("\n");
+            System.out.println("1. Create Airline");
+            System.out.println("2. Update Airline");
+            System.out.println("3. Delete Airline");
+            System.out.println("4. Get Airline by Id");
+            System.out.println("5. Get Airline by Name");
+            System.out.println("6. Get All Airlines");
+            System.out.println("0. Exit");
 
-      switch (scanner.nextInt()) {
-        case 1:
-          createAirline(scanner);
-          break;
-        case 2:
-          updateAirline(scanner);
-          break;
-        case 3:
-          deleteAirline(scanner);
-          break;
-        case 4:
-          getAirlineById(scanner);
-          break;
-        case 5:
-          getAirlineByName(scanner);
-          break;
-        case 6:
-          getAllAirlines();
-          break;
-        case 7:
-          exit();
-          break;
-
-        default:
-          break;
-      }
+            switch (scanner.nextInt()) {
+                case 1:
+                    createAirline(scanner);
+                    break;
+                case 2:
+                    updateAirline(scanner);
+                    break;
+                case 3:
+                    deleteAirline(scanner);
+                    break;
+                case 4:
+                    getAirlineById(scanner);
+                    break;
+                case 5:
+                    getAirlineByName(scanner);
+                    break;
+                case 6:
+                    getAllAirlines();
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
-  }
 
-  public void createAirline(Scanner scanner) {
-    System.out.println("Enter Airline Name");
-    String name = scanner.nextLine();
-    Airline airline = new Airline(name);
-    airlineService.createAirline(airline);
-  }
+    public void createAirline(Scanner scanner) {
+        System.out.println("Enter Airline Name");
+        String name = scanner.nextLine();
+        Airline airline = new Airline(name);
+        airlineService.createAirline(airline);
+    }
 
-  public void updateAirline(Scanner scanner) {
-    System.out.println("Enter Airline Id");
-    Long id = scanner.nextLong();
-    System.out.println("Enter new Airline Name");
-    String name = scanner.nextLine();
-    Airline airline = new Airline(id, name);
-    airlineService.updateAirline(airline);
-  }
+    public void updateAirline(Scanner scanner) {
+        System.out.println("Enter Airline Id");
+        Long id = scanner.nextLong();
+        System.out.println("Enter new Airline Name");
+        String name = scanner.nextLine();
+        Airline airline = new Airline(id, name);
+        airlineService.updateAirline(airline);
+    }
 
-  public void deleteAirline(Scanner scanner) {
-    System.out.println("Enter Airline Id");
-    Long id = scanner.nextLong();
-    airlineService.deleteAirlineById(id);
-  }
+    public void deleteAirline(Scanner scanner) {
+        System.out.println("Enter Airline Id");
+        Long id = scanner.nextLong();
+        airlineService.deleteAirlineById(id);
+    }
 
-  public void getAirlineById(Scanner scanner) {
-    System.out.println("Enter Airline Id");
-    Long id = scanner.nextLong();
-    System.out.println(airlineService.getAirlineById(id));
-  }
+    public void getAirlineById(Scanner scanner) {
+        System.out.println("Enter Airline Id");
+        Long id = scanner.nextLong();
+        System.out.println(airlineService.getAirlineById(id));
+    }
 
-  public void getAirlineByName(Scanner scanner) {
-    System.out.println("Enter Airline Name");
-    String name = scanner.nextLine();
-    System.out.println(airlineService.getAirlinesByName(name));
-  }
+    public void getAirlineByName(Scanner scanner) {
+        System.out.println("Enter Airline Name");
+        String name = scanner.nextLine();
+        System.out.println(airlineService.getAirlinesByName(name));
+    }
 
-  public void getAllAirlines() {
-    System.out.println(airlineService.getAllAirlines());
-  }
-
-  public void exit() {
-    System.exit(0);
-  }
-
+    public void getAllAirlines() {
+        System.out.println(airlineService.getAllAirlines());
+    }
 }
--- src/main/java/com/vuelosjanbi/crewRole/infrastructure/adapters/in/CrewRoleConsoleAdapter.java
+++ src/main/java/com/vuelosjanbi/crewRole/infrastructure/adapters/in/CrewRoleConsoleAdapter.java
@@ -10,60 +10,61 @@ import com.vuelosjanbi.crewRole.domain.models.CrewRole;
 @Controller
 public class CrewRoleConsoleAdapter {
 
-  @Autowired
-  private CrewRoleService crewRoleService;
+    @Autowired
+    private CrewRoleService crewRoleService;
 
-  public void start() {
+    public void start() {
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-      System.out.println("\n");
-      System.out.println("1. Create Crew Role.");
-      System.out.println("2. Update Crew Role.");
-      System.out.println("3. Delete Crew Role.");
-      System.out.println("4. List all Crew Roles.");
-      System.out.println("0. Exit.");
-      switch (scanner.nextInt()) {
-        case 1:
-          System.out.println("Type the name of the crew role:");
-          String crewRoleName = scanner.nextLine();
+        while (true) {
+            System.out.println("\n");
+            System.out.println("1. Create Crew Role.");
+            System.out.println("2. Update Crew Role.");
+            System.out.println("3. Delete Crew Role.");
+            System.out.println("4. List all Crew Roles.");
+            System.out.println("0. Exit.");
 
-          crewRoleService.createCrewRole(new CrewRole(crewRoleName));
-          break;
-        case 2:
-          System.out.println("Choose the crew role you want to modify:");
-          crewRoleService.getAllCrewRoles().forEach(crewRole -> {
-            System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
-          });
-          Long updateCrewRoleId = scanner.nextLong();
-          scanner.nextLine();
-          System.out.println("Type the new name of the crew role:");
-          String newCrewRoleName = scanner.nextLine();
-          CrewRole updatedCrewRole = new CrewRole(updateCrewRoleId, newCrewRoleName);
-          crewRoleService.updateCrewRole(updatedCrewRole);
-          break;
-        case 3:
-          System.out.println("Choose the crew role you want to delete:");
-          crewRoleService.getAllCrewRoles().forEach(crewRole -> {
-            System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
-          });
-          Long deleteCrewRoleId = scanner.nextLong();
-          scanner.nextLine();
-          crewRoleService.deleteCrewRole(deleteCrewRoleId);
-          break;
-        case 4:
-          crewRoleService.getAllCrewRoles().forEach(crewRole -> {
-            System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
-          });
-          break;
-        case 0:
-          System.out.println("Exiting...");
-          return;
-        default:
-          System.out.println("Invalid choice. Please try again.");
-          break;
-      }
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("Type the name of the crew role:");
+                    String crewRoleName = scanner.nextLine();
+
+                    crewRoleService.createCrewRole(new CrewRole(crewRoleName));
+                    break;
+                case 2:
+                    System.out.println("Choose the crew role you want to modify:");
+                    crewRoleService.getAllCrewRoles().forEach(crewRole -> {
+                        System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
+                    });
+                    Long updateCrewRoleId = scanner.nextLong();
+                    scanner.nextLine();
+                    System.out.println("Type the new name of the crew role:");
+                    String newCrewRoleName = scanner.nextLine();
+                    CrewRole updatedCrewRole = new CrewRole(updateCrewRoleId, newCrewRoleName);
+                    crewRoleService.updateCrewRole(updatedCrewRole);
+                    break;
+                case 3:
+                    System.out.println("Choose the crew role you want to delete:");
+                    crewRoleService.getAllCrewRoles().forEach(crewRole -> {
+                        System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
+                    });
+                    Long deleteCrewRoleId = scanner.nextLong();
+                    scanner.nextLine();
+                    crewRoleService.deleteCrewRole(deleteCrewRoleId);
+                    break;
+                case 4:
+                    crewRoleService.getAllCrewRoles().forEach(crewRole -> {
+                        System.out.printf("ID: %d  Name: %s\n", crewRole.getId(), crewRole.getName());
+                    });
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
-  }
 }
